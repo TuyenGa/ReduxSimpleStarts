9dcb923d68e675dfb32c8c1db73f1350e2245b69
VuongQuangTuyen <TuyenGa@techmasters-iMac-8.local>
2017-12-08 17:30:28 +0700

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default () =>{
+    return [
+        {title: 'Javascript : A good parts'},
+        {title: 'Harry Potter'},
+        {title: 'The Dark Tower' },
+        {title: 'Eloquent Ruby'}
+    ]
+}
